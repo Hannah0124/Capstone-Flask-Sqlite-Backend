b4faafa6bcf953efbdf617dbe2348018839f94bd
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-09 07:24:34 -0700

    able to add user to db

--- api/views.py
+++ api/views.py
@@ -1,9 +1,25 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from . import db
+from .models import User
+from .models import Image
 
 main = Blueprint('main', __name__)
 
 @main.route('/add_user', methods=['POST'])
 def add_user():
+  # json - now a dictionary
+  # take the data from require
+  user_data = request.get_json()
+
+  new_user = User(
+    uid=user_data['uid'],
+    provider=user_data['provider'],
+    username=user_data['username'],
+    email=user_data['email']
+    )
+  # adding to the database and commit
+  db.session.add(new_user)
+  db.session.commit()
 
   return 'Adding user', 201
 
