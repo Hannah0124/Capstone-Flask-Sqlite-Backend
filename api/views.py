ccd80061f68594507538d43c55f8bd5c2b9555ff
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-09 15:14:20 -0700

    changed views version

--- api/views.py
+++ api/views.py
@@ -1,62 +1,68 @@
+  
 from flask import Blueprint, jsonify, request
-from . import db
-from .models import User
+from . import db 
 from .models import Image
+from .models import User 
 
 main = Blueprint('main', __name__)
 
-@main.route('/add_user', methods=['POST'])
-def add_user():
-  # json - now a dictionary
-  # take the data from require
-  user_data = request.get_json()
+# endpoint
+@main.route('/add_image', methods=['POST'])
 
-  new_user = User(
-    uid=user_data['uid'],
-    provider=user_data['provider'],
-    username=user_data['username'],
-    email=user_data['email']
-    )
-  # adding to the database and commit
-  db.session.add(new_user)
+def add_image():
+  image_data = request.get_json()
+
+  # create new image object
+  new_image = Image(
+    image_url=image_data['image_url'], 
+    text=image_data['text'], 
+    translated_text=image_data['translated_text'], favorite=image_data['favorite'],
+    language=image_data['language'],
+    user_id=image_data['user_id']
+  )
+  
+  db.session.add(new_image)
   db.session.commit()
 
-  return 'Adding user', 201
 
+  return 'Done', 201  # success
 
-# to save the user session in order to get the user image?
-@main.route('/saved_images')
-def saved_images():
+# endpoint
+@main.route('/images')
 
+def images():
   image_list = Image.query.all()
   images = []
 
   for image in image_list:
-    images.append({
-      'words': image.words,
-      'translatedWords': image.translatedWords,
-      'photo_url': image.photo_url,
+    images.append({ 
+      'image_url': image.image_url, 
+      'text': image.text, 
+      'translated_text': image.translated_text,
       'favorite': image.favorite,
-      'user_id': image.user_id
-      })
+      'language': image.language,
+      'user_id': image.user_id 
+    })
 
-  return jsonify({"images": images})
+  return jsonify({'images' : images})
 
-# should be nested routes? or save user in session ?
-# example for nested route - "/api/movies/<int:movie_id>/cast_members" 
-@main.route('/add_image')
-def add_image():
-  image_data = request.get_json()
 
-  new_image = Image(
-    words=image_data['words'],
-    translatedWords=image_data['translatedWords'],
-    photo_url=image_data['photo_url'],
-    favorite=image_data['favorite'],
-    user_id=image_data['user_id'] # should be pass as params?
+# endpoint
+@main.route('/add_user', methods=['POST'])
+
+def add_user():
+  user_data = request.get_json()
+
+  # create new user object
+  new_user = User(
+    uid=user_data['uid'], 
+    provider=user_data['provider'], 
+    username=user_data['username'], 
+    email=user_data['email']
   )
-
-  db.session.add(new_image)
+  
+  db.session.add(new_user)
   db.session.commit()
 
-  return 'Adding image', 201
+
+  return 'Done', 201  # success
