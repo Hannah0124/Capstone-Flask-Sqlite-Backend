57d6992d352a6bd35ab7de9721844d3269eec90a
Hannah <hannahjoo24@gmail.com>
2020-07-10 22:31:11 -0700

    Update create_app() in __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,14 +1,36 @@
 from flask import Flask
+from os import environ
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from .commands import create_users, create_database
+from .extensions import db, guard
+from .models import User
+from .routes import api
+
+# db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
 
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+  # print("!!!")
+  # print(app.config)
+
+  app.config['SECRET_KEY'] = 'sdflkjweiofjwefijsldkjflsdfjk' #environ.get('SECRET_KEY')
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #  
+  app.config['JWT_ACCESS_LIFESPAN'] = {'minutes' : 1}
+
+  # They way below is not working (TODO)
+  # app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+  # app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI') 
+
 
   db.init_app(app)
+  guard.init_app(app, User)
+
+  app.cli.add_command(create_users)
+  app.cli.add_command(create_database)
+
+  app.register_blueprint(api)
 
   from .views import main
   app.register_blueprint(main)
