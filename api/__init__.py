d15aa29c91619cbbebf254de198e3895d5c32c27
Sharon Cheung <keikei1128@g.ucla.edu>
2020-07-14 13:00:52 -0700

    small change on secret key

--- api/__init__.py
+++ api/__init__.py
@@ -17,8 +17,8 @@ def create_app():
   # print("!!!")
   # print(app.config)
 
-  # app.config['SECRET_KEY'] = 'sdflkjweiofjwefijsldkjflsdfjk' #environ.get('SECRET_KEY')
-  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+  app.config['SECRET_KEY'] = 'sdflkjweiofjwefijsldkjflsdfjk' #environ.get('SECRET_KEY')
+  # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
   app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nlmviczenkhkjl:6a2e77c6f597095382f1e070b66a82d08ea068c1cd95bce7729669813bbcdc29@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dbanpqa83pbt1i' #  
   # app.config['SQLALCHEMY_DATABASE_URI']='postgresql://localhost/mydb'
   app.config['JWT_ACCESS_LIFESPAN'] = {'minutes' : 1}
