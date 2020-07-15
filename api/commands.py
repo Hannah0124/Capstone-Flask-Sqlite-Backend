b3f7519bab5b00e2ecb6abb23ee041057f18b891
Hannah <hannahjoo24@gmail.com>
2020-07-15 02:22:38 -0700

    Change uid type in commands.py

--- api/commands.py
+++ api/commands.py
@@ -12,9 +12,9 @@ def create_database():
 @click.command(name='create_users')
 @with_appcontext
 def create_users():
-  one = User(uid=123, provider="Google", username='One', password=guard.hash_password('one'))
-  two = User(uid=456, provider="Google", username='Two', password=guard.hash_password('two'))
-  three = User(uid=789, provider="Google", username='Three', password=guard.hash_password('three'))
+  one = User(uid="123", provider="Google", username='One', password=guard.hash_password('one'))
+  two = User(uid="456", provider="Google", username='Two', password=guard.hash_password('two'))
+  three = User(uid="789", provider="Google", username='Three', password=guard.hash_password('three'))
 
   db.session.add_all([one, two, three])
   db.session.commit()
