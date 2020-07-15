f58b6e76af98516a24abb576639c3d21c4d54c34
Hannah <hannahjoo24@gmail.com>
2020-07-15 02:14:18 -0700

    Clean code in __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -9,17 +9,15 @@ from .extensions import db, guard
 from .models import User
 from .routes import api
 
-# db = SQLAlchemy()
-
 def create_app():
   app = Flask(__name__)
 
-  # print("!!!")
   # print(app.config)
 
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
   app.config['SECRET_KEY'] = 'sdflkjweiofjwefijsldkjflsdfjk' 
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lvwbjxtmssmblc:4e742b0d00d6535331089cb52d42ef34118516fe398660b45167fd96a2b66ac7@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d874b4ihi94mvj'  # 'sqlite:///database.db'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lvwbjxtmssmblc:4e742b0d00d6535331089cb52d42ef34118516fe398660b45167fd96a2b66ac7@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d874b4ihi94mvj'  
+  # 'sqlite:///database.db'
   app.config['JWT_ACCESS_LIFESPAN'] = {'minutes' : 1}
 
   # They way below is not working (TODO)
